9dc00df3e21ec16db8082cd1c68a2e162c1aa83a
huangdexin <735513870@qq.com>
2019-02-26 11:43:54 +0800

    basic-demo init

---
+++ src/main/java/top/atstudy/basic/collection/CollectionTest.java
@@ -0,0 +1,24 @@
+package top.atstudy.basic.collection;
+
+import java.util.ArrayList;
+
+/**
+ * @author huangdexin @ harley
+ * @email huangdexin@kuaicto.com
+ * @date 2019/2/26 11:37
+ */
+public class CollectionTest {
+
+    public static void ArrayListTest(){
+        ArrayList<String> list = new ArrayList<>();
+
+
+
+        list.iterator();
+
+
+
+    }
+
+
+}
