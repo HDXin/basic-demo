df6534ab789f9c174f80112fba7b85092206cb45
huangdexin <735513870@qq.com>
2019-02-26 13:46:36 +0800

    测试

---
+++ src/test/java/top/atstudy/basic/serializable/SerializableTest.java
@@ -0,0 +1,13 @@
+package top.atstudy.basic.serializable;
+
+/**
+ * @author huangdexin @ harley
+ * @email huangdexin@kuaicto.com
+ * @date 2019/2/26 11:52
+ */
+public class SerializableTest {
+
+
+
+
+}
