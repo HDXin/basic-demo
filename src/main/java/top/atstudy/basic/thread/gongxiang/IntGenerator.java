aeaddbeae0528c6e2092b72283e52aeed047f620
huangdexin <735513870@qq.com>
2019-04-21 23:54:35 +0800

    共享资源

---
+++ src/main/java/top/atstudy/basic/thread/gongxiang/IntGenerator.java
@@ -0,0 +1,12 @@
+package top.atstudy.basic.thread.gongxiang;
+
+public abstract class IntGenerator {
+    private volatile boolean canceled = false;
+    public abstract int next();
+    public void cancel(){
+        canceled = true;
+    }
+    public boolean isCanceled(){
+        return canceled;
+    }
+}
