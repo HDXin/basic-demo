dd44c968aa081f17772c68a42517e9902fd39ceb
huangdexin <735513870@qq.com>
2020-03-04 15:01:09 +0800

    数据库连接池c3p0/dbcp/druid

--- src/main/java/top/atstudy/basic/jdbc/druid/DemoToDruid.java
+++ src/main/java/top/atstudy/basic/jdbc/druid/DemoToDruid.java
@@ -1,22 +1,24 @@
 package top.atstudy.basic.jdbc.druid;
 
-import java.io.IOException;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import javax.sql.DataSource;
 import java.util.Properties;
 
 public class DemoToDruid {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         getConnection();
 
     }
 
-    private static void getConnection() throws IOException {
+    private static void getConnection() throws Exception {
 
         Properties properties = new Properties();
         properties.load(ClassLoader.getSystemResourceAsStream("druid.properties"));
 
-//        DataSource dataSource = DruidDataSou
+        DataSource dataSource = new DruidDataSourceFactory().createDataSource(properties);
+        System.out.println(" ===>> conn: " + dataSource.getConnection().toString());
 
     }
 
