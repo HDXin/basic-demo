8377770b9eaf8dd419eb04b3e9861fc428aa78b5
harley <735513870@qq.com>
2021-04-14 21:17:23 +0800

    test

--- src/main/java/top/atstudy/basic/netty/nio/buffer/BufferTest.java
+++ src/main/java/top/atstudy/basic/netty/nio/buffer/BufferTest.java
@@ -1,40 +1,141 @@
 package top.atstudy.basic.netty.nio.buffer;
 
+import cn.hutool.core.util.RandomUtil;
+
 import java.nio.IntBuffer;
-import java.util.Random;
 
 /**
  * int mark = -1;
  * int position = 0;
- * int limit;
- * int capacity;
+ * int limit = 0;
+ * int capacity ;
  */
 public class BufferTest {
 
+    private static IntBuffer buf;
+
+    static {
+        buf = IntBuffer.allocate(10);
+    }
+
     public static void main(String[] args) {
 
-        IntBuffer buf = IntBuffer.allocate(5);
+        // test put
+//        putTest();
 
-        System.out.println(" ====>> put2 ");
+        // test get
+//        getTest();
+
+        // test get
+//        getTest2();
+
+        // test slice
+//        sliceTest();
+
+        // test duplicate
+//        duplicateTest();
+
+        // test compact
+        compactTest();
+
+    }
+
+    public static void compactTest() {
+
+        putTest();
+        buf.flip();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(buf.get());
+        }
+        System.out.println(" ========= ");
+
+        IntBuffer temp = buf.compact();
+        while (temp.hasRemaining()) {
+            System.out.println(temp.get());
+        }
+
+        System.out.println(" ==>> compact finish ... ");
+
+
+    }
+
+    /**
+     * 复制一份
+     */
+    public static void duplicateTest() {
+
+        putTest();
+        buf.flip();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(buf.get());
+        }
+        System.out.println(" ========= ");
+
+        IntBuffer temp = buf.duplicate();
+        while (temp.hasRemaining()) {
+            System.out.println(temp.get());
+        }
+
+        System.out.println(" ==>> duplicate finish ... ");
+
+    }
+
+    /**
+     * 截取剩余有效数据
+     */
+    public static void sliceTest() {
+        putTest();
+        buf.flip();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(buf.get());
+        }
+        System.out.println(" ========= ");
+
+        IntBuffer temp = buf.slice();
+        while (temp.hasRemaining()) {
+            System.out.println(temp.get());
+        }
+
+        System.out.println(" ==>> slice finish ... ");
+
+    }
+
+    public static void getTest2() {
+
+        putTest();
+        buf.flip();
+
+        while (buf.hasRemaining()) {
+            System.out.println(buf.get());
+        }
+
+        System.out.println(" ==>> get finish ... ");
+
+    }
+
+    public static void getTest() {
+
+        putTest();
+        buf.flip();
+
+        for (int i = 0; i < buf.limit(); i++) {
+            System.out.println(buf.get(i));
+        }
+
+        System.out.println(" ==>> get finish ... ");
+
+    }
+
+    public static void putTest() {
+
         for (int i = 0; i < buf.capacity() - 3; i++) {
-            buf.put(new Random().nextInt(10));
+            buf.put(RandomUtil.randomInt(10, 20));
         }
 
-        buf.flip();
-        System.out.println(" ====>> get2 ");
-        for (int i = 0; i < buf.limit(); i++) {
-            System.out.println(buf.get(i));
-        }
-
-        System.out.println(" ====>> put5 ");
-        buf.flip();
-        buf.put(new Random().nextInt(100));
-
-        buf.flip();
-        System.out.println(" ====>> get5 ");
-        for (int i = 0; i < buf.limit(); i++) {
-            System.out.println(buf.get(i));
-        }
+        System.out.println(" ==>> put finish ... ");
 
     }
 
---
+++ src/main/java/top/atstudy/basic/netty/nio/buffer/DirectBufferTest.java
@@ -0,0 +1,16 @@
+package top.atstudy.basic.netty.nio.buffer;
+
+/**
+ * @Author: dexin.huang or harley
+ * @Email: dexin.huang@paat.com
+ * @Date: 2021/4/14 21:16
+ * @Description
+ */
+public class DirectBufferTest {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
