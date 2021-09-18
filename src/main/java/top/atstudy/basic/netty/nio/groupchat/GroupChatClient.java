d2a63e928afe8e6d315d163a552ad0224425391c
harley <735513870@qq.com>
2021-09-18 10:01:14 +0800

    test

--- src/main/java/top/atstudy/basic/netty/nio/groupchat/GroupChatClient.java
+++ src/main/java/top/atstudy/basic/netty/nio/groupchat/GroupChatClient.java
@@ -11,28 +11,28 @@ import java.util.Scanner;
 
 public class GroupChatClient {
 
-    //定义相关属性
+    // 定义相关属性
     private final String HOST = "127.0.0.1";
     private final int PORT = 8888;
     private Selector selector;
     private SocketChannel socketChannel;
     private String username;
 
-    //构造器，完成初始化工作
+    // 构造器，完成初始化工作
     public GroupChatClient() throws IOException {
         selector = Selector.open();
-        //连接服务器
+        // 连接服务器
         socketChannel = socketChannel.open(new InetSocketAddress(HOST, PORT));
-        //设置非阻塞
+        // 设置非阻塞
         socketChannel.configureBlocking(false);
-        //将 channel 注册到 selector
+        // 将 channel 注册到 selector
         socketChannel.register(selector, SelectionKey.OP_READ);
-        //得到 username
+        // 得到 username
         username = socketChannel.getLocalAddress().toString().substring(1);
         System.out.println(username + " is ok ... ");
     }
 
-    //向服务器发送消息
+    // 向服务器发送消息
     public void sendInfo(String info) {
         info = username + " say: " + info;
         try {
@@ -42,24 +42,24 @@ public class GroupChatClient {
         }
     }
 
-    //读取从服务器端回复的消息
+    // 读取从服务器端回复的消息
     public void readInfo() {
         try {
             int readChannels = selector.select();
-            if (readChannels > 0) { //有可以用的通道
+            if (readChannels > 0) { // 有可以用的通道
 
                 Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
                 while (iterator.hasNext()) {
 
                     SelectionKey key = iterator.next();
                     if (key.isReadable()) {
-                        //得到相关的通道
+                        // 得到相关的通道
                         SocketChannel sc = (SocketChannel) key.channel();
-                        //得到一个 Buffer
+                        // 得到一个 Buffer
                         ByteBuffer buffer = ByteBuffer.allocate(1024);
-                        //读取
+                        // 读取
                         sc.read(buffer);
-                        //把读到的缓存区数据转换成字符串
+                        // 把读到的缓存区数据转换成字符串
                         String msg = new String(buffer.array());
                         System.out.println(msg.toString());
                     }
