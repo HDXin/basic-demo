60fb12d772ee7c073d4bc13f1f3a2457ea876442
harley <735513870@qq.com>
2024-11-25 10:59:23 +0800

    jdk动态代理

--- src/main/java/top/atstudy/basic/designmode/proxy/jdk/DemoProxyClient.java
+++ src/main/java/top/atstudy/basic/designmode/proxy/jdk/DemoProxyClient.java
@@ -8,18 +8,15 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-/**
- * @Author: dexin.huang or harley
- * @Email: dexin.huang@paat.com
- * @Date: 2021/10/13 11:27
- * @Desc: xxx
- */
 public class DemoProxyClient {
 
     public static void main(String[] args) {
 
         //
-//        demo2();
+//        demo();
+
+        //
+        demo2();
 
         //
 //        demo3();
@@ -27,7 +24,7 @@ public class DemoProxyClient {
         //
 //        demo4();
 
-        demo5();
+//        demo5();
 
     }
 
@@ -83,10 +80,9 @@ public class DemoProxyClient {
     public static void demo2(){
         MyInvocationHandler invocation = new MyInvocationHandler();
         Auth auth = new AuthImpl();
-        Auth tempProxy = (Auth) invocation.bind(auth);
-        final Auth temp = tempProxy;
+        final Auth temp = auth;
 
-        tempProxy = (Auth) Proxy.newProxyInstance(tempProxy.getClass().getClassLoader(), new Class[]{Auth.class}, new InvocationHandler() {
+        auth = (Auth) Proxy.newProxyInstance(auth.getClass().getClassLoader(), new Class[]{Auth.class}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 System.out.println("abc before ... ");
@@ -97,19 +93,32 @@ public class DemoProxyClient {
             }
         });
 
-        final Auth temp2 = tempProxy;
-        tempProxy = (Auth) Proxy.newProxyInstance(tempProxy.getClass().getClassLoader(), new Class[]{Auth.class}, new InvocationHandler() {
+        final Auth temp2 = auth;
+        auth = (Auth) Proxy.newProxyInstance(auth.getClass().getClassLoader(), new Class[]{Auth.class}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-                System.out.println(" 111 before ... ");
+                System.out.println(" 1111 before ... ");
                 Object result = method.invoke(temp2, args);
                 System.out.println(" 1111 after ... ");
 
                 return result;
             }
         });
-        System.out.println(tempProxy.add(1, 2));
+        int result = auth.add(1, 2);
+        System.out.println("===>> 1 + 2 = " + result);
+    }
+
+
+    public static void demo(){
+
+        MyInvocationHandler invocation = new MyInvocationHandler();
+        Auth auth = new AuthImpl();
+        Auth proxy = (Auth) invocation.bind(auth);
+
+        int result = proxy.add(2, 3);
+        System.out.println(" ===>> 2 + 3 = " + result);
+
     }
 
 }
